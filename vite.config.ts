d3852aaafbc2f6e3dffb9fddd96a7be4cc61b3aa
Tony <t4tonykuriakose@gmail.com>
2025-03-19 13:56:53 +0530

    set up:configure tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()], server:{port:3000},
+  plugins: [react(),tailwindcss()], server:{port:3000},
 })
